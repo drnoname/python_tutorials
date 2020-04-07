206b1ef8ed3fb172eed5c61b65062a9947608c88
drnoname <46642584+drnoname@users.noreply.github.com>
2020-04-07 21:53:42 +0800

    update performance analysis

---
+++ performance/02_performance_analysis/fibonacci.py
@@ -0,0 +1,15 @@
+def fibonacci(n): 
+    if n<0: 
+        print("Incorrect input") 
+    # First Fibonacci number is 0 
+    elif n==1: 
+        return 0
+    # Second Fibonacci number is 1 
+    elif n==2: 
+        return 1
+    else: 
+        return fibonacci(n-1) + fibonacci(n-2) 
+    
+    
+if __name__ == "__main__":
+    fibonacci(35)
